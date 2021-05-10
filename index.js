755633922510cde239ea76a79ea39ab33c4abaea
tommy-diez <td275@njit.edu>
2021-05-10 11:00:17 -0400

    Task: built reg page, handled registration POST request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const handleBars = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 const app = express();
 
@@ -12,8 +13,8 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 
 app.engine('hbs', handleBars({
-      extname: '.hbs'
-    }));
+  extname: '.hbs'
+}));
 
 app.set('view engine', 'hbs');
 
@@ -22,3 +23,62 @@ app.listen(3000);
 app.get('/', function(req, res) {
   res.render('home');
 })
+
+app.get('/registration', function(req, res) {
+  res.render('registration');
+})
+
+function getHashedPassword(password) {
+  const hash = crypto.createHash('sha256');
+  const hashedPassword = hash.update(password).digest('base64');
+  return hashedPassword;
+}
+
+const users = [{
+  firstName: 'Admin',
+  lastName: 'Admin',
+  email: 'admin@localhost.com',
+  password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+}];
+
+app.post('/registration', function(req, res) {
+  const {
+    email,
+    firstName,
+    lastName,
+    password,
+    confirmPassword
+  } = req.body;
+
+  if (password === confirmPassword) {
+    for (user of users) {
+      if (user.email === email) {
+        res.render('registration', {
+          message: 'User already registered',
+          messageClass: 'alert-danger'
+        });
+        return;
+      }
+
+      const hashedPassword = getHashedPassword(password);
+
+      users.push({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword
+      });
+
+      res.render('login', {
+        message: 'Registration Complete. Please login to continue.',
+            messageClass: 'alert-success'
+      })
+
+    }
+  } else {
+    res.render('registration', {
+      message: 'Passwords do not match',
+      messageClass: 'alert-danger'
+    });
+  }
+});
